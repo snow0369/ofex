d23107129f5efdcdc6768b36aeb6e1ade47cb9d1
Gwonhak Lee <sh_ow@naver.com>
2024-07-23 23:12:36 +0900

    l2min done

--- algorithms/funcapprox/func_plt.py
+++ algorithms/funcapprox/func_plt.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional, Sequence
+from typing import Dict, Optional, Sequence, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,13 +7,29 @@ import sympy as sp
 
 def plot_functions(func_list: Dict[str, sp.Expr],
                    x: sp.Symbol,
-                   x_points: Optional[Sequence[float]] = None):
+                   x_points: Optional[Sequence[float]] = None,
+                   title: Optional[str] = None,):
     if x_points is None:
         x_points = np.linspace(-1, 1, 100)
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    plt_imag = False
+    y_points = dict()
     for func_name, func in func_list.items():
-        y_points = sp.lambdify(x, func, "numpy")(x_points)
-        ax.plot(x_points, y_points, label=func_name)
-    ax.legend()
+        y_points[func_name] = sp.lambdify(x, func, "numpy")(x_points)
+        if not np.allclose(y_points[func_name].imag, 0.0):
+            plt_imag = True
+
+    fig = plt.figure()
+    if not plt_imag:
+        ax = fig.add_subplot(111)
+        for func_name, func in func_list.items():
+            ax.plot(x_points, y_points[func_name].real, label=func_name)
+        ax.legend()
+    else:
+        axes = fig.subplots(2)
+        for func_name, func in func_list.items():
+            axes[0].plot(x_points, y_points[func_name].real, label=func_name)
+            axes[1].plot(x_points, y_points[func_name].imag, label=func_name)
+        axes[1].legend()
+    if title is not None:
+        plt.title(title)
     plt.show()

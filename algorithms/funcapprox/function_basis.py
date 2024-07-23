a868d48963e13ca61b8314f343ed64be917d20c2
Gwonhak Lee <sh_ow@naver.com>
2024-07-23 23:12:36 +0900

    l2min done

--- algorithms/funcapprox/function_basis.py
+++ algorithms/funcapprox/function_basis.py
@@ -5,9 +5,10 @@ from typing import List, Callable, Optional, Tuple, Union, Sequence
 
 import numpy as np
 import sympy as sp
+from scipy.integrate import quad
 
 
-def _uniform_inner_product(x_max, a: sp.Expr, b: sp.Expr) -> complex:
+def _uniform_inner_product(x_max, numerical_integ, a: sp.Expr, b: sp.Expr) -> complex:
     prod = sp.simplify((a.conjugate() * b))
     syms = prod.free_symbols
     if len(syms) == 0:
@@ -15,11 +16,19 @@ def _uniform_inner_product(x_max, a: sp.Expr, b: sp.Expr) -> complex:
     elif len(syms) > 1:
         raise ValueError(f"There are redundant symbols {prod}, {syms}")
     x = list(syms)[0]
-    return complex(sp.integrate(prod, (x, -x_max, x_max)).evalf() / (2 * x_max))
+    if not numerical_integ:
+        try:
+            return complex(sp.integrate(prod, (x, -x_max, x_max)).evalf() / (2 * x_max))
+        except TypeError:
+            pass
+    integrand = sp.lambdify(x, prod, "numpy")
+    integral, _ = quad(integrand, -x_max, x_max, complex_func=True)
+    return complex(integral / (2 * x_max))
 
 
-def uniform_inner_product(x_max: float) -> Callable[[sp.Expr, sp.Expr], complex]:
-    return partial(_uniform_inner_product, x_max)
+def uniform_inner_product(x_max: float,
+                          numerical_integ: bool) -> Callable[[sp.Expr, sp.Expr], complex]:
+    return partial(_uniform_inner_product, x_max, numerical_integ)
 
 
 class FunctionBasis(object):
@@ -49,9 +58,13 @@ class FunctionBasis(object):
         s = self.overlap_matrix
         mu = self.projection(func)
         c = np.linalg.solve(s, mu)
-        diff = self.inner_product(func, func) - c.T.conj() @ mu
-        assert np.isclose(diff.imag, 0.0), diff
         opt_g: sp.Expr = sum([cf * b for cf, b in zip(c, self.basis)])
+        func_norm_2 = self.inner_product(func, func)
+
+        diff = (func_norm_2 - np.dot(mu.conj(), c))
+        assert np.isclose(diff.imag, 0.0), diff
+        if diff < 0 and np.isclose(diff, 0.0):
+            diff = 0.0
         return opt_g, c, np.sqrt(diff.real)
 
     def l2_minimization_regularized_1(self,
@@ -77,7 +90,6 @@ class FunctionBasis(object):
         for n_it in range(max_iter):
             mu_tilde = mu_tilde_0 - diff0 * reg_coeff
             c_abs = np.linalg.solve(s_tilde, mu_tilde)
-            assert np.allclose(c_abs / np.abs(c_abs), 1.0)
             c = c_abs * c0_phase
 
             dot = np.dot(c.conj(), mu)
@@ -92,13 +104,51 @@ class FunctionBasis(object):
         return opt_g, c, np.sqrt(diff.real)
 
 
-def uniform_fourier_basis(x_max: float, n_harmonics: int) -> Tuple[FunctionBasis, sp.Symbol, np.ndarray]:
-    inner_product = uniform_inner_product(x_max)
-    x = sp.Symbol('x')
+def uniform_fourier_basis(x_max: float, n_harmonics: int, deriv_order: int = 0,
+                          numerical_integ: bool = False,
+                          sym_x: Optional[sp.Symbol] = None,
+                          debug=False) \
+        -> Tuple[FunctionBasis, sp.Symbol, np.ndarray]:
+    inner_product = uniform_inner_product(x_max, numerical_integ)
+    if sym_x is None:
+        sym_x = sp.Symbol('x')
     delta_omega = np.pi / x_max
     omega_list = [delta_omega * k for k in range(-n_harmonics, n_harmonics + 1)]
-    basis = [sp.exp(1j * omega * x) for omega in omega_list]
-    return FunctionBasis(basis, inner_product), x, np.array(omega_list)
+    basis = [sp.exp(1j * omega * sym_x) for omega in omega_list]
+    for d in range(deriv_order):
+        basis += [(sym_x ** (d + 1)) * sp.exp(1j * omega * sym_x) for omega in omega_list]
+
+    w_len = len(omega_list)
+    overlap_matrix = np.zeros((len(basis), len(basis)), dtype=complex)
+    for (idx_w1, w1), (idx_w2, w2) in product(enumerate(omega_list), repeat=2):
+        for d1, d2 in product(range(deriv_order + 1), repeat=2):
+            idx_b1 = idx_w1 + d1 * w_len
+            idx_b2 = idx_w2 + d2 * w_len
+            if idx_b1 > idx_b2:
+                continue
+            overlap_matrix[idx_b1, idx_b2] = _monomial_fourier_integral(w2 - w1, d1 + d2, -x_max, x_max) / (2 * x_max)
+
+    for idx_b1, idx_b2 in product(range(len(basis)), repeat=2):
+        if idx_b1 > idx_b2:
+            overlap_matrix[idx_b1, idx_b2] = overlap_matrix[idx_b2, idx_b1].conjugate()
+
+    fb = FunctionBasis(basis, inner_product)
+    if debug:
+        assert np.allclose(fb.overlap_matrix, overlap_matrix)
+    else:
+        fb._overlap_matrix = overlap_matrix
+    return fb, sym_x, np.array(omega_list)
+
+
+def _monomial_fourier_integral(omega, n, a, b):
+    # integrate x^n e^{iωx} from x=-a to b.
+    if np.isclose(omega, 0):
+        return (b ** (n + 1) - a ** (n + 1)) / (n + 1)
+    elif n == 0:
+        return (np.exp(1j * omega * b) - np.exp(1j * omega * a)) / (1j * omega)
+    else:
+        res = (np.exp(1j * omega * b) * b ** n - np.exp(1j * omega * a) * a ** n) / (1j * omega)
+        return res - _monomial_fourier_integral(omega, n - 1, a, b) * (n / (1j * omega))
 
 
 if __name__ == '__main__':
@@ -106,10 +156,28 @@ if __name__ == '__main__':
 
 
     def function_basis_test():
-        f_basis, x, omega_list = uniform_fourier_basis(x_max=1.0, n_harmonics=4)
-        gaussian = sp.exp(-x ** 2)
-        opt_g, c, df = f_basis.l2_minimization(gaussian)
-        plot_functions({'target': gaussian, 'approx': opt_g}, x)
+        f_basis_0, x, omega_list = uniform_fourier_basis(x_max=1.0, n_harmonics=4, deriv_order=0,
+                                                         numerical_integ=True,
+                                                         debug=False)
+        f_basis_1, x, omega_list = uniform_fourier_basis(x_max=1.0, n_harmonics=4, deriv_order=1,
+                                                         numerical_integ=True,
+                                                         sym_x=x,
+                                                         debug=False)
+        gaussian = sp.exp(- x ** 2)
+        opt_g_0, c_0, df_0 = f_basis_0.l2_minimization(gaussian)
+        opt_g_1, c_1, df_1 = f_basis_1.l2_minimization(gaussian)
+        plot_functions({'target': gaussian, 'approx_0': opt_g_0, 'qpprox_1': opt_g_1}, x)
+        print(c_0)
+        print(c_1)
+        print(df_0, df_1)
+
+        for samp_err in [0.1, 0.01, 0.001]:
+            opt_g_0, c_0, df_0 = f_basis_0.l2_minimization_regularized_1(gaussian, samp_err)
+            opt_g_1, c_1, df_1 = f_basis_1.l2_minimization_regularized_1(gaussian, samp_err)
+            plot_functions({'target': gaussian, 'approx_0': opt_g_0, 'qpprox_1': opt_g_1}, x, title=f"{samp_err}")
+            print(c_0)
+            print(c_1)
+            print(df_0, df_1)
 
 
     function_basis_test()

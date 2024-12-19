d9ba04e6cb1c77f490338e0d2f24d63e05f827f7
Gwonhak Lee <sh_ow@naver.com>
2024-12-20 00:21:24 +0900

    minor changes

--- ofex/measurement/killer_shift.py
+++ ofex/measurement/killer_shift.py
@@ -46,8 +46,8 @@ def killer_shift_opt_fermion_hf(fham: FermionOperator,
         optimization_level (int, optional): The optimization level used in the killer 
             shift procedure. Available options (default: 1):
             - 0: Use the number operator only.
-            - 1: Include contributions from one-body operators.
-            - 2: Include contributions from both one- and two-body operators.
+            - 1: Include contributions from both one- and two-body operators with efficient optimization.
+            - 2: Include contributions from both one- and two-body operators with numerical optimization.
         repeat_opt (int, optional): The number of optimization iterations to 
             perform for refining the solution (default: 1).
         f2q_kwargs (Optional[Dict[str, Any]], optional): Additional keyword arguments 

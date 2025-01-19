f8942d8f54e99b65e731c88cd0fae64e88cbb9ed
Gwonhak Lee <sh_ow@naver.com>
2025-01-20 00:24:24 +0900

    found problems in regularized approximation

--- ofex/classical_algorithms/funcapprox/function_basis.py
+++ ofex/classical_algorithms/funcapprox/function_basis.py
@@ -178,56 +178,115 @@ class FunctionBasis(object):
 
         return opt_list
 
-    def l2_minimization_regularized_1(self,
-                                      func: sp.Expr,
-                                      reg_coeff: Union[Sequence[float], float],
-                                      max_iter: int = 1000,
-                                      conv_atol: float = 1e-8) \
+    def l2_minimization_regularized(self,
+                                    func: sp.Expr,
+                                    reg_coeff: Union[Sequence[float], float],
+                                    order: int,
+                                    max_iter: int = 1000,
+                                    conv_atol: float = 1e-6) \
             -> Tuple[sp.Expr, np.ndarray, float]:
         """
         Performs L2-minimization with a regularization term.
-    
+        
+        This method finds an optimal linear combination of basis functions with regularization. 
+        Different methods are utilized for first and second-order minimization.
+        
+        For 1st order regularization:
+        
+        .. math::
+            \min_{\vec{c}} \left[\|f\|^2 + \vec{c}^{\dagger}\mathbf{S}\vec{c} 
+            - (\vec{c}^{\dagger}\vec{\mu} + \vec{\mu}^{\dagger}\vec{c})\right]^{1/2} 
+            + \sum_k |c_k| \|\epsilon_k\|.
+        
+        The solution satisfies:
+        
+        .. math::
+            \mathbf{S} \vec{c}^{\mathrm{(opt)}} = \vec{\mu} - \vec{\delta}(\vec{c}^{\mathrm{(opt)}}).
+        
+        An iterative method is used to achieve convergence.
+        
+        For 2nd order regularization:
+        
+        .. math::
+            \min_{\vec{c}} \left[\|f(x)\|^2 + \vec{c}^{\dagger}\mathbf{S}\vec{c}
+            - (\vec{c}^{\dagger}\vec{\mu} + \vec{\mu}^{\dagger}\vec{c}) 
+            + \vec{c}^{\dagger} \mathbf{E} \vec{c} \right]^{1/2},
+        
+        which leads to the linear equation:
+        
+        .. math::
+            (\mathbf{S} + \mathbf{E}) \vec{c} = \vec{\mu}.
+        
         Args:
             func (sp.Expr): The target function for minimization.
-            reg_coeff (Union[Sequence[float], float]): Regularization coefficients.
-            max_iter (int): Maximum number of iterations (default=1000).
-            conv_atol (float): Convergence absolute tolerance (default=1e-8).
-    
+            reg_coeff (Union[Sequence[float], float]): Regularization coefficients. 
+                Positive values only (either a single value or a sequence matching the basis size).
+            order (int): Specifies the regularization order (1 or 2).
+            max_iter (int, optional): Maximum number of iterations for the 1st order optimization 
+                (default is 1000).
+            conv_atol (float, optional): Convergence absolute tolerance for the iterative method 
+                (default is 1e-6).
+        
         Returns:
-            Tuple[sp.Expr, np.ndarray, float]: 
-                - The optimal function approximation.
+            Tuple[sp.Expr, np.ndarray, float]: A tuple containing:
+                - The optimal function approximation as a symbolic expression.
                 - Coefficients for the linear combination of basis functions.
-                - Regularized residual error norm of the approximation.
+                - The residual error norm of the approximation.
+        
+        Raises:
+            ValueError: If any regularization coefficient is non-positive.
+            NotImplementedError: If the specified regularization order is not 1 or 2.
         """
         if isinstance(reg_coeff, Number):
             reg_coeff = np.array([reg_coeff for _ in range(self.n_basis)])
+        if not np.all(np.array(reg_coeff) > 0):
+            raise ValueError("All regularization coefficients (reg_coeff) must be positive.")
         func_norm_2 = self.inner_product(func, func)
-        _, c0, diff0 = self.l2_minimization(func)
-        c0_phase = c0 / np.abs(c0)
-
         s = self.overlap_matrix
-        s_tilde = np.real(np.diag(c0_phase).conj() @ s @ np.diag(c0_phase))
-
         mu = self.projection(func)
-        mu_tilde_0 = np.real(np.diag(c0_phase).conj() @ mu)
 
-        diff, c = diff0, c0
-        for n_it in range(max_iter):
-            mu_tilde = mu_tilde_0 - diff0 * reg_coeff
-            c_abs = np.linalg.solve(s_tilde, mu_tilde)
-            c = c_abs * c0_phase
+        if order == 1:
+            c = np.linalg.solve(s, mu)
+            diff = (func_norm_2 - np.dot(mu.conj(), c))
+            assert np.isclose(diff.imag, 0.0), diff
+            if diff < 0 and np.isclose(diff, 0.0):
+                diff = 0.0
+            diff = np.sqrt(diff)
 
-            dot = np.dot(c.conj(), mu)
-            diff = (func_norm_2 + c.conj().T @ s @ c - dot - dot.conjugate()) ** (1 / 2) + np.dot(c_abs, reg_coeff)
-            if abs(diff - diff0) < conv_atol:
-                break
-            diff0 = diff
+            c_phase = c / np.abs(c)
+
+            mu_tilde = mu - diff * c_phase * reg_coeff
+
+            for n_it in range(max_iter):
+                c = np.linalg.solve(s, mu_tilde)
+                diff = (func_norm_2 + c.conj().T @ s @ c - 2 * np.dot(c.conj(), mu).real) ** (1 / 2)
+                c_phase = c / np.abs(c)
+
+                # Update new mu_tilde and check the difference between Sc and mu_tilde
+                mu_tilde = mu - diff * c_phase * reg_coeff
+                delta = (s @ c) - mu_tilde
+                print(n_it, np.linalg.norm(delta))
+                if np.linalg.norm(delta) < conv_atol:
+                    break
+            else:
+                print("Reached max iter")
+
+        elif order == 2:
+            e_mat = np.outer(reg_coeff, reg_coeff)
+            c = np.linalg.solve(s + e_mat, mu)
+            diff = (func_norm_2 - np.dot(c.conj(), s @ c))
+
+            if diff < 0 and np.isclose(diff, 0.0):
+                diff = 0.0
+            assert np.isclose(diff.imag, 0.0) and diff.real >= 0.0, diff
+            diff = np.sqrt(diff.real)
+
         else:
-            print("Reached max iter")
+            raise NotImplementedError("Only order 1 and 2 are supported.")
 
         assert np.isclose(diff.imag, 0.0), diff
         opt_g = sp.Add(*(cf * b for cf, b in zip(c, self.basis)))
-        return opt_g, c, np.sqrt(diff.real)
+        return opt_g, c, diff.real
 
 
 class FourierBasis(FunctionBasis):
@@ -357,8 +416,8 @@ if __name__ == '__main__':
         print(df_0, df_1)
 
         for samp_err in [0.1, 0.01, 0.001]:
-            opt_g_0, c_0, df_0 = f_basis_0.l2_minimization_regularized_1(gaussian, samp_err)
-            opt_g_1, c_1, df_1 = f_basis_1.l2_minimization_regularized_1(gaussian, samp_err)
+            opt_g_0, c_0, df_0 = f_basis_0.l2_minimization_regularized(gaussian, samp_err)
+            opt_g_1, c_1, df_1 = f_basis_1.l2_minimization_regularized(gaussian, samp_err)
             plot_functions({'target': gaussian, 'approx_0': opt_g_0, 'qpprox_1': opt_g_1}, x, title=f"{samp_err}")
             print(c_0)
             print(c_1)
